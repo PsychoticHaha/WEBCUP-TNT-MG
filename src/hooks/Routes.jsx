d37dcc065b9523ae2c1cc8a5f222d584883d32f0
Fanasina Ramalandimanana <hope.fanasina2@gmail.com>
2024-05-04 18:51:49 +0300

    Resolve Routes export

--- src/hooks/Routes.jsx
+++ src/hooks/Routes.jsx
@@ -1,12 +1,13 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import App from "../App";
+import { RouterProvider, createBrowserRouter, Route } from "react-router-dom";
+import App from "./../App";
 import Login from './../pages/Login/Login'
-import SignUp from './../pages/Login/SignUp'
+import SignUp from './../pages/SignUp/SignUp'
 
-const router = createBrowserRouter({
-  path: '/',
-  element: <App />,
-},
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
   {
     path: "/login",
     element: <Login />
@@ -15,13 +16,12 @@ const router = createBrowserRouter({
     path: "/signup",
     element: <SignUp />
   },
-)
-function Routes({ children }) {
+])
+export default function Routes({ children }) {
+
   return (
     <RouterProvider router={router}>
-      {children}
+      <Route element={children} />
     </RouterProvider>
   );
 }
-
-export default Routes;
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Routes } from 'react-router-dom'
+import Routes from './hooks/Routes.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
