643852681f18d54bf057d71345897208ec369c23
Fanasina Ramalandimanana <hope.fanasina2@gmail.com>
2024-05-04 16:00:30 +0300

    One hour Commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="/" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
       </div>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+
+function Navbar() {
+  return (
+    <div>
+      <h1>NAVBAR FROM FANASINA</h1>
+    </div>
+  );
+}
+
+export default Navbar;
